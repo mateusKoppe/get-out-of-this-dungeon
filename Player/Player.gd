39feaf91558da7f80856edbdeffee5663a7d07b6
Mateus Koppe <mateus.koppe@gmail.com>
2020-08-29 13:37:56 -0300

    Player attack animations

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
 export var MAX_SPEED = 80
-export var ACCELERATION = 8
-export var FRICTION = 10
+export var ACCELERATION = 12
+export var FRICTION = 20
 
 enum {
 	MOVE,
@@ -12,6 +12,7 @@ enum {
 var state = MOVE
 var move_direction = Vector2.ZERO
 var input_direction = Vector2.ZERO
+var face_direction = Vector2.RIGHT
 
 func get_input_direction():
 	input_direction = Vector2.ZERO
@@ -36,11 +37,23 @@ func state_move():
 		move_direction = move_direction.move_toward(input_direction * MAX_SPEED, FRICTION)
 	else:
 		move_direction = move_direction.move_toward(input_direction * MAX_SPEED, ACCELERATION)
-		$Sprite.flip_h = move_direction.x < 0
+		face_direction = Vector2.LEFT if move_direction.x < 0 else Vector2.RIGHT
+		
+	if face_direction == Vector2.LEFT:
+		$AnimationPlayer.play("IdleLeft")
+	else:
+		$AnimationPlayer.play("IdleRight")
 		
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 		
 func state_attack():
+	if face_direction == Vector2.LEFT:
+		$AnimationPlayer.play("AttackLeft")
+	else:
+		$AnimationPlayer.play("AttackRight")
+	
 	move_direction = move_direction.move_toward(Vector2.ZERO * MAX_SPEED, FRICTION)
+	
+func on_attack_animation_finished():
 	state = MOVE
